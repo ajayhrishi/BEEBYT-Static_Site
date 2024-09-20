a5fa907e2f3d273d71200d5f3c476b733000acd2
Ajay Hrishi <ajayhrishi@gmail.com>
2024-09-21 00:43:09 +0530

    fixed the font styles issue of the our Team page

--- src/styles/our_team_page_styles.jsx
+++ src/styles/our_team_page_styles.jsx
@@ -15,17 +15,24 @@ export const our_team_page_styles = {
 
   top_div: `h-[auto]`,
   ceo_details: `h-[auto] w-[auto]`,
-  ceo_card: `flex mt-[5vh]`,
+  ceo_card: `flex mt-[5vh] ph:items-center ph:mr-[10px]`,
 
-  CEO_picture: `${an} h-[15vh] w-[auto] rounded-[50%]`,
+  CEO_picture: `${an} h-[15vh] phl:h-[30vh] w-[auto] rounded-[50%]`,
   name_des: `${an} flex-col ml-[2vw]`,
-  ceo_name: `${an} text-[3vw] hover:text-[#18ff8e]`,
-  designation_name: `${an} text-[2vw] hover:text-[#18ff8e] `,
-  ceo_words: `${an} w-[40vw] text-wrap m-[2vw] text-[1vw] hover:text-[#18ff8e]`,
+  ceo_name: `${an} hover:text-[#18ff8e]
+  text-[3vw] ph:text-[6vw] phl:text-[3vw] tb:text-[6vw] tbl:text-[3vw]
+  `,
+  designation_name: `${an} hover:text-[#18ff8e] 
+   text-[2vw] ph:text-[4vw] tb:text-[3vw]
+  `,
+  ceo_words: `${an} w-[40vw] text-wrap m-[2vw] hover:text-[#18ff8e]
+   text-[1vw] ph:text-[3vw] ph:mt-[10px] tb:text-[2vw] tbl:text-[2vw]
+  `,
 
-  location_container: `flex ml-[80%] items-center justify-center`,
-  location_icon: `${an} h-[1vw] w-[auto] `,
-  location_typo: `${an} ml-[10px] text-[1vw]  hover:text-[#18ff8e]`,
+  location_container: `flex ml-[80%] items-center justify-center  ph:mt-[10px] tb:min-w-[10vw]`,
+  location_icon: `${an} h-[1vw] ph:h-[3vw] tb:h-[3vw] tbl:h-[2vw] w-[auto] `,
+  location_typo: `${an} ml-[10px] text-[1vw]  hover:text-[#18ff8e] 
+  ph:text-[3vw] tb:text-[2vw] tbl:text-[2vw] min-w-[100px]`,
 
   bottom_div: ``,
   small_title: `text-[1vw] ph:text-[2vh] tb:text-[2vh] tbl:text-[2vw] phl:text-[2vh]`,
